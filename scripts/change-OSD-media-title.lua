3e76d93617d82789870dade5e7c0cb772a05f6e9
andrizan <putra7a@gmail.com>
2024-11-25 22:46:07 +0700

    update scripts: * autoload.lua * playlistmanager.lua * pause-when-minimize.lua * delete_current_file.lua * change-OSD-media-title.lua

--- scripts/change-OSD-media-title.lua
+++ scripts/change-OSD-media-title.lua
@@ -3,11 +3,16 @@ function set_osd_title()
 	local percent_pos = ""
 	local chapter = ""
 	local frames_dropped = ""
-	
+	local pl = ""
+
+	if mp.get_property_number("playlist-count", 0) > 1 then
+		pl = "[" .. mp.get_property_number("playlist-pos", 0) + 1 .. "/" .. mp.get_property_number("playlist-count", 0) .."] "
+	end
+
 	if mp.get_property_osd("percent-pos") ~= "" then
 		percent_pos = " • " .. mp.get_property_osd("percent-pos") .. "% completed"
 	end
-	
+
 	if mp.get_property_osd("chapter") ~= "" then
 		chapter = " • Chapter: " .. mp.get_property_osd("chapter")
 	end
@@ -20,9 +25,11 @@ function set_osd_title()
 		end
 	end
 
-	mp.set_property("force-media-title", name .. percent_pos .. chapter .. frames_dropped)	
+	mp.set_property("force-media-title", pl .. name .. percent_pos .. chapter .. frames_dropped)
 end
 
+mp.observe_property("playlist-count", "number", set_osd_title)
+mp.observe_property("playlist-pos", "number", set_osd_title)
 mp.observe_property("percent-pos", "number", set_osd_title)
 mp.observe_property("chapter", "string", set_osd_title)
 mp.observe_property("frame-drop-count", "number", set_osd_title)
